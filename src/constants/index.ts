bb7eb5afea0d5454a966bd9453a16917bbcb4909
Udit Kumar <uditkumarussa@gmail.com>
2022-08-08 02:46:05 +0530

    copy changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ export const cachedSites: Record<string, any> = {
     description:
       "Amazon.com is the world's largest online marketplace, and the largest online destination for consumers to shop for, find and compare prices for products, and buy directly from sellers. Amazon.com is the best place to buy, sell, and save money on everything you want.",
     image:
-      "http://g-ec2.images-amazon.com/images/G/31/img14/anywhere/amazon-logo-500500._V327001990_.jpg",
+      "https://i0.wp.com/www.badtaste.it/videogiochi/wp-content/uploads/sites/2/2019/11/amazon-banner.jpg?fit=1000%2C500&quality=85&strip=all&ssl=1",
     favicon: "https://amazon.com/favicon.ico",
     keywords: "amazon, amazon.com, online shopping, online store, e-commerce",
     url: "https://www.amazon.com",
@@ -14,7 +14,7 @@ export const cachedSites: Record<string, any> = {
     description:
       "Amazon.com is the world's largest online marketplace, and the largest online destination for consumers to shop for, find and compare prices for products, and buy directly from sellers. Amazon.com is the best place to buy, sell, and save money on everything you want.",
     image:
-      "http://g-ec2.images-amazon.com/images/G/31/img14/anywhere/amazon-logo-500500._V327001990_.jpg",
+      "https://i0.wp.com/www.badtaste.it/videogiochi/wp-content/uploads/sites/2/2019/11/amazon-banner.jpg?fit=1000%2C500&quality=85&strip=all&ssl=1",
     favicon: "https://amazon.com/favicon.ico",
     keywords: "amazon, amazon.com, online shopping, online store, e-commerce",
     url: "https://www.amazon.com",
@@ -35,12 +35,11 @@ export const cachedSites: Record<string, any> = {
       "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!",
     description:
       "India's biggest online store for Mobiles, Fashion (Clothes/Shoes), Electronics, Home Appliances, Books, Home, Furniture, Grocery, Jewelry, Sporting goods, Beauty & Personal Care and more! Find the largest selection from all brands at the lowest prices in India. Payment options - COD, EMI, Credit card, Debit card &amp; more.",
-    image:
-      "https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png",
+    image: "https://1000logos.net/wp-content/uploads/2021/02/Flipkart-logo.png",
     favicon:
       "https://static-assets-web.flixcart.com/www/promos/new/20150528-140547-favicon-retina.ico",
     keywords: "flipkart, online shopping, online store, e-commerce",
-    url: "https://flipkart.com",
+    url: "https://flipkart.in",
   },
   "google.com": {
     title: "Google: Don't be evil",
